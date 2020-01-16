0e5c152434da1e2dba3f37902aeb92063f162944
Charles Wahome <thewahome.cw@gmail.com>
2020-01-16 17:11:26 +0300

    tests adding students and student exists

--- StudentList.Services/StudentManager.cs
+++ StudentList.Services/StudentManager.cs
@@ -1,29 +1,49 @@
 ﻿using System;
+using System.Linq;
 
 namespace StudentList.Services
 {
     public class StudentManager
     {
         private StudentStorage _studentStorage;
-        public StudentManager()
-        {
-            _studentStorage = new StudentStorage();
+        private const char _studentEntryDelimiter = ',';
+        private Random _rand;
+        private string _studentList;
+
+        public string[] Students { 
+            get
+            {
+                return _studentList.Split(_studentEntryDelimiter);
+            }
         }
 
         public StudentManager(StudentStorage studentStorage)
         {
             _studentStorage = studentStorage;
-        }
-        public string[] GetAllStudents()
-        {
-            var studentList = _studentStorage.LoadStudentsList();
-            return studentList.Split(',');
+            _rand = new Random();
+            _studentList = _studentStorage.LoadStudentsList();
+
         }
 
-        public int CountStudents()
+        public string PickRandomStudent() 
         {
-            var studentList = _studentStorage.LoadStudentsList();
-            return studentList.Split(',').Length;        
+            var randomIndex = _rand.Next(0, this.Students.Length-1);
+            return this.Students[randomIndex];
+        }
+
+        public bool StudentExists(string student)
+        {
+            if (this.Students.Any(k=>k.Trim().ToLower() == student.ToLower()))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void AddStudent(string newStudent)
+        {
+            _studentList += "," + newStudent;
+           _studentStorage.UpdateStudentList(_studentList);
         }
     }
 }
--- StudentList.Tests/StudentManager_Should.cs
+++ StudentList.Tests/StudentManager_Should.cs
@@ -20,7 +20,7 @@ namespace StudentList.Tests
         public void ReturnAListOfStudents()
         {
             var sut = new StudentManager(mockStorage.Object);
-            var actual = sut.GetAllStudents();
+            var actual = sut.Students;
             Assert.IsType(typeof(string[]), actual);
             Assert.True(actual.Length == 3);
         }
@@ -29,8 +29,44 @@ namespace StudentList.Tests
         public void ReturnCorrectStudentCount()
         {
             var sut = new StudentManager(mockStorage.Object);
-            var actual = sut.CountStudents();
+            var actual = sut.Students.Length;
             Assert.True(actual == 3);
+        }
+
+        [Fact]
+        public void ReturnRandomStudent()
+        {
+            var sut = new StudentManager(mockStorage.Object);
+            var students = sut.Students;
+
+            var expectedStudent = sut.PickRandomStudent();
+            Assert.Contains(expectedStudent, students);
+
+        }
+
+        [Fact]
+        public void ReturnFalse_when_SeacrchingForExistingStudent ()
+        {
+            var sut = new StudentManager(mockStorage.Object);
+            var fakeStudent = "STUDENT1";
+            var students = sut.Students;
+
+            var actual = sut.StudentExists(fakeStudent);
+            Assert.True(actual);
+
+        }
+
+        [Fact]
+        public void Call_UpdateStudentList_When_StudentAdded()
+        {
+            var sut = new StudentManager(mockStorage.Object);
+            var originalList = mockStorage.Object.LoadStudentsList();
+            var newStudent = "newStudent";
+            var updatedList = originalList + "," + newStudent;
+
+            sut.AddStudent(newStudent);
+
+            mockStorage.Verify(x=>x.UpdateStudentList(updatedList));
         }
     }
 }
